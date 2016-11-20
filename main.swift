5a8307e467b631bd3142d8fcb86d27ff031fa297
Timofey Solomko <tsolomko@gmail.com>
2016-11-20 20:20:37 +0300

    Added program itself.

---
+++ main.swift
@@ -0,0 +1,22 @@
+import SWCompression
+import Foundation
+
+do {
+  let archType = CommandLine.arguments[1]
+  let fileData = try Data(contentsOf: URL(fileURLWithPath: CommandLine.arguments[2]))
+  let decompressedData: Data
+  switch archType {
+  case "bzip2":
+    decompressedData = try BZip2.decompress(compressedData: fileData)
+  case "gzip":
+    decompressedData = try GzipArchive.unarchive(archiveData: fileData)
+  default:
+    print("ERROR: unknown archive type.")
+    exit(1)
+  }
+  try decompressedData.write(to: URL(fileURLWithPath: "./data.out"))
+} catch let error {
+  print("ERROR: \(error)")
+  exit(1)
+}
+exit(0)
