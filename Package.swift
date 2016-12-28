c6623876c73073a1e2f62ed24623bf2194db1250
Timofey Solomko <tsolomko@gmail.com>
2016-12-28 22:57:04 +0300

    Updated package.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "swcomp",
     dependencies: [
-        .Package(url: "https://github.com/tsolomko/SWCompression.git", majorVersion: 1),
+        .Package(url: "https://github.com/tsolomko/SWCompression.git", majorVersion: 2),
     ]
 )
