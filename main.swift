7f79e4858645dcbcc2452c738c2e2402f4e87f76
Timofey Solomko <tsolomko@gmail.com>
2017-01-01 18:18:30 +0300

    Fixed indentation.

--- main.swift
+++ main.swift
@@ -1,28 +1,31 @@
 import SWCompression
 import Foundation
 
+/* TODO: Add '--version' command-line option
+when Bundle.allBundles() function of Foundation framework becomes implemented.*/
+
 do {
-  let archType = CommandLine.arguments[1]
+    let archType = CommandLine.arguments[1]
     let fileData = try Data(contentsOf: URL(fileURLWithPath: CommandLine.arguments[2]),
                             options: .mappedIfSafe)
-  let outputPath = CommandLine.arguments[3]
-  let decompressedData: Data
-  switch archType {
-  case "lzma":
-    decompressedData = try LZMA.decompress(compressedData: fileData)
-  case "xz":
-    decompressedData = try XZArchive.unarchive(archiveData: fileData)
-  case "bzip2":
-    decompressedData = try BZip2.decompress(compressedData: fileData)
-  case "gzip":
-    decompressedData = try GzipArchive.unarchive(archiveData: fileData)
-  default:
-    print("ERROR: unknown archive type.")
-    exit(1)
-  }
-  try decompressedData.write(to: URL(fileURLWithPath: outputPath))
+    let outputPath = CommandLine.arguments[3]
+    let decompressedData: Data
+    switch archType {
+    case "lzma":
+        decompressedData = try LZMA.decompress(compressedData: fileData)
+    case "xz":
+        decompressedData = try XZArchive.unarchive(archiveData: fileData)
+    case "bzip2":
+        decompressedData = try BZip2.decompress(compressedData: fileData)
+    case "gzip":
+        decompressedData = try GzipArchive.unarchive(archiveData: fileData)
+    default:
+        print("ERROR: unknown archive type.")
+        exit(1)
+    }
+    try decompressedData.write(to: URL(fileURLWithPath: outputPath))
 } catch let error {
-  print("ERROR: \(error)")
-  exit(1)
+    print("ERROR: \(error)")
+    exit(1)
 }
 exit(0)
