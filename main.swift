9d0f46238db3d9277b35a817fa5c8f241b36b38d
Timofey Solomko <tsolomko@gmail.com>
2017-06-02 00:16:07 +0300

    [rev: 43] Added tar BZip2 command.

--- main.swift
+++ main.swift
@@ -5,7 +5,7 @@ import SwiftCLI
 /* TODO: Switch to usage of Bundle.allBundles() function of Foundation framework when it becomes implemented.*/
 // Version constants:
 let SWCompressionVersion = "3.0.0"
-let swcompRevision = "42"
+let swcompRevision = "43"
 
 class XZCommand: Command {
 
@@ -250,6 +250,44 @@ class TxzCommand: Command {
 
 }
 
+class Tbz2Command: Command {
+
+    let name = "tbz2"
+    let shortDescription = "Extracts TAR container compressed with BZip2"
+
+    let archive = Parameter()
+    let outputPath = Parameter()
+
+    func execute() throws {
+        let fileData = try Data(contentsOf: URL(fileURLWithPath: self.archive.value),
+                                options: .mappedIfSafe)
+        let containerData = try BZip2.decompress(data: fileData)
+        let outputPath = self.outputPath.value
+        let entries = try TarContainer.open(container: containerData)
+        for entry in entries {
+            let entryName = entry.name
+            if entry.isDirectory {
+                let directoryURL = URL(fileURLWithPath: outputPath)
+                    .appendingPathComponent(entryName, isDirectory: true)
+                if verbose.value {
+                    print("directory: \(directoryURL.path)")
+                }
+                try FileManager.default.createDirectory(at: directoryURL,
+                                                        withIntermediateDirectories: true)
+            } else {
+                let entryData = try entry.data()
+                let fileURL = URL(fileURLWithPath: outputPath)
+                    .appendingPathComponent(entryName, isDirectory: false)
+                if verbose.value {
+                    print("file: \(fileURL.path)")
+                }
+                try entryData.write(to: fileURL)
+            }
+        }
+    }
+
+}
+
 struct SWCompGlobalOptions: GlobalOptionsSource {
     static let verbose = Flag("--verbose", usage: "Print the list of extracted files and directories.")
     static var options: [Option] {
@@ -281,6 +319,12 @@ extension TxzCommand {
     }
 }
 
+extension Tbz2Command {
+    var verbose: Flag {
+        return SWCompGlobalOptions.verbose
+    }
+}
+
 CLI.setup(name: "swcomp",
           version: "\(swcompRevision), SWCompression version: \(SWCompressionVersion)",
           description: "swcomp - small command-line client for SWCompression framework.")
@@ -293,5 +337,6 @@ CLI.register(commands: [XZCommand(),
                         ZipCommand(),
                         TarCommand(),
                         TgzCommand(),
-                        TxzCommand()])
+                        TxzCommand(),
+                        Tbz2Command()])
 _ = CLI.go()
