b2fa2fbb775fa4e8ccc95cc33767a1e1b7b7e437
Timofey Solomko <tsolomko@gmail.com>
2017-07-04 23:22:22 +0300

    [rev: 47] Added creation of symlinks and applying of attributes in ZIP.

--- Sources/ZipCommand.swift
+++ Sources/ZipCommand.swift
@@ -11,28 +11,92 @@ class ZipCommand: Command {
     let outputPath = Parameter()
 
     func execute() throws {
+        let fileManager = FileManager.default
+
+        let outputPath = self.outputPath.value
+        let outputURL = URL(fileURLWithPath: outputPath)
+
+        var isDir: ObjCBool = false
+        if fileManager.fileExists(atPath: outputPath, isDirectory: &isDir) {
+            // TODO: probably will need to use isDir on linux.
+            if !isDir.boolValue {
+                print("ERROR: Specified path already exists and is not a directory.")
+                exit(1)
+            }
+        } else {
+            try fileManager.createDirectory(at: outputURL, withIntermediateDirectories: true)
+        }
+
         let fileData = try Data(contentsOf: URL(fileURLWithPath: self.archive.value),
                                 options: .mappedIfSafe)
-        let outputPath = self.outputPath.value
-        let zipList = try ZipContainer.open(container: fileData)
-        for entry in zipList {
-            let entryName = entry.name
-            if entry.isDirectory {
-                let directoryURL = URL(fileURLWithPath: outputPath)
-                    .appendingPathComponent(entryName, isDirectory: true)
+        let entries = try ZipContainer.open(container: fileData)
+
+        for entry in entries {
+            let attributes = entry.entryAttributes
+            guard let type = attributes[FileAttributeKey.type] as? FileAttributeType else {
+                print("ERROR: Not a FileAttributeType type. This error should never happen.")
+                exit(1)
+            }
+
+            let isDirectory = type == FileAttributeType.typeDirectory || entry.isDirectory
+
+            let entryPath = entry.name
+            let entryFullURL = outputURL.appendingPathComponent(entryPath, isDirectory: isDirectory)
+
+            if isDirectory {
                 if verbose.value {
-                    print("directory: \(directoryURL.path)")
+                    print("directory: \(entryPath)")
                 }
-                try FileManager.default.createDirectory(at: directoryURL,
-                                                        withIntermediateDirectories: true)
-            } else {
+                try fileManager.createDirectory(at: entryFullURL, withIntermediateDirectories: true)
+            } else if type == FileAttributeType.typeRegular {
+                if verbose.value {
+                    print("file: \(entryPath)")
+                }
                 let entryData = try entry.data()
-                let fileURL = URL(fileURLWithPath: outputPath)
-                    .appendingPathComponent(entryName, isDirectory: false)
+                try entryData.write(to: entryFullURL)
+            } else if type == FileAttributeType.typeSymbolicLink {
+                // Data of entry is a relative path from the directory in which entry is located to destination.
                 if verbose.value {
-                    print("file: \(fileURL.path)")
+                    print("symbolic link: \(entryPath)", terminator: "")
                 }
-                try entryData.write(to: fileURL)
+                let entryData = try entry.data()
+                guard let destinationPath = String(data: entryData, encoding: .utf8) else {
+                    print("ERROR: Unable to get destination path for symbolic link \(entryPath).")
+                    exit(1)
+                }
+                let endURL = entryFullURL.deletingLastPathComponent().appendingPathComponent(destinationPath)
+                if verbose.value {
+                    print(" destination: \(endURL.path)")
+                }
+                try fileManager.createSymbolicLink(atPath: entryFullURL.path, withDestinationPath: endURL.path)
+                // We cannot apply attributes to symbolic link.
+                continue
+            } else {
+                print("WARNING: Unknown file type \(type) for entry \(entryPath). Skipping this entry.")
+            }
+
+            var attributesLog = "\tattributes:"
+
+            if let mtime = attributes[FileAttributeKey.modificationDate] {
+                attributesLog += " mtime: \(mtime)"
+                try fileManager.setAttributes([FileAttributeKey.modificationDate : mtime],
+                                              ofItemAtPath: entryFullURL.path)
+            }
+
+            if let permissions = attributes[FileAttributeKey.posixPermissions] {
+                attributesLog += " permissions: \(permissions)"
+                try fileManager.setAttributes([FileAttributeKey.posixPermissions : permissions],
+                                              ofItemAtPath: entryFullURL.path)
+            }
+
+            if let readOnly = attributes[FileAttributeKey.appendOnly] {
+                attributesLog += " read-only"
+                try fileManager.setAttributes([FileAttributeKey.appendOnly : readOnly],
+                                              ofItemAtPath: entryFullURL.path)
+            }
+
+            if verbose.value {
+                print(attributesLog)
             }
         }
     }
