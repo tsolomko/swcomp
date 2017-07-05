71ec6126770a290e46f9cd979663541992969926
Timofey Solomko <tsolomko@gmail.com>
2017-07-05 19:21:20 +0300

    [rev: 56] Added new tar features to other tar commands.

--- Sources/Tbz2Command.swift
+++ Sources/Tbz2Command.swift
@@ -12,6 +12,8 @@ class Tbz2Command: Command {
     let name = "tbz2"
     let shortDescription = "Extracts TAR container compressed with BZip2"
 
+    let noMtime = Flag("--no-restore-mtime", usage: "Don't restore modification time of files and directories.")
+
     let archive = Parameter()
     let outputPath = Parameter()
 
@@ -19,28 +21,7 @@ class Tbz2Command: Command {
         let fileData = try Data(contentsOf: URL(fileURLWithPath: self.archive.value),
                                 options: .mappedIfSafe)
         let containerData = try BZip2.decompress(data: fileData)
-        let outputPath = self.outputPath.value
-        let entries = try TarContainer.open(container: containerData)
-        for entry in entries {
-            let entryName = entry.name
-            if entry.isDirectory {
-                let directoryURL = URL(fileURLWithPath: outputPath)
-                    .appendingPathComponent(entryName, isDirectory: true)
-                if verbose.value {
-                    print("directory: \(directoryURL.path)")
-                }
-                try FileManager.default.createDirectory(at: directoryURL,
-                                                        withIntermediateDirectories: true)
-            } else {
-                let entryData = try entry.data()
-                let fileURL = URL(fileURLWithPath: outputPath)
-                    .appendingPathComponent(entryName, isDirectory: false)
-                if verbose.value {
-                    print("file: \(fileURL.path)")
-                }
-                try entryData.write(to: fileURL)
-            }
-        }
+        try TarCommand.process(tarContainer: containerData, outputPath.value, noMtime.value, verbose.value)
     }
 
 }
--- Sources/TxzCommand.swift
+++ Sources/TxzCommand.swift
@@ -12,35 +12,16 @@ class TxzCommand: Command {
     let name = "txz"
     let shortDescription = "Extracts TAR container compressed with XZ"
 
+    let noMtime = Flag("--no-restore-mtime", usage: "Don't restore modification time of files and directories.")
+
     let archive = Parameter()
     let outputPath = Parameter()
 
     func execute() throws {
         let fileData = try Data(contentsOf: URL(fileURLWithPath: self.archive.value),
                                 options: .mappedIfSafe)
-        let containerData = try XZArchive.unarchive(archive: fileData)
-        let outputPath = self.outputPath.value
-        let entries = try TarContainer.open(container: containerData)
-        for entry in entries {
-            let entryName = entry.name
-            if entry.isDirectory {
-                let directoryURL = URL(fileURLWithPath: outputPath)
-                    .appendingPathComponent(entryName, isDirectory: true)
-                if verbose.value {
-                    print("directory: \(directoryURL.path)")
-                }
-                try FileManager.default.createDirectory(at: directoryURL,
-                                                        withIntermediateDirectories: true)
-            } else {
-                let entryData = try entry.data()
-                let fileURL = URL(fileURLWithPath: outputPath)
-                    .appendingPathComponent(entryName, isDirectory: false)
-                if verbose.value {
-                    print("file: \(fileURL.path)")
-                }
-                try entryData.write(to: fileURL)
-            }
-        }
+        let containerData = try BZip2.decompress(data: fileData)
+        try TarCommand.process(tarContainer: containerData, outputPath.value, noMtime.value, verbose.value)
     }
 
 }
