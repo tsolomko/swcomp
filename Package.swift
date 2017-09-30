1441b19c6250c8591309c65ef7e48e5f3a0f5ddf
Timofey Solomko <tsolomko@gmail.com>
2017-09-30 23:39:15 +0300

    Fix (literally) versions of SWCompression and SwiftCLI

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "swcomp",
     dependencies: [
         .Package(url: "https://github.com/tsolomko/SWCompression", Version(3, 3, 0, prereleaseIdentifiers: ["test"])),
-        .Package(url: "https://github.com/jakeheis/SwiftCLI", majorVersion: 3)
+        .Package(url: "https://github.com/jakeheis/SwiftCLI", majorVersion: 3, minor: 0)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -9,8 +9,8 @@ import SwiftCLI
 
 /* TODO: Switch to usage of Bundle.allBundles() function of Foundation framework when it becomes implemented.*/
 // Version constants:
-let SWCompressionVersion = "3.3.0-test.2"
-let swcompRevision = "83"
+let SWCompressionVersion = "3.3.0"
+let swcompRevision = "84"
 
 CLI.setup(name: "swcomp",
           version: "\(swcompRevision), SWCompression version: \(SWCompressionVersion)",
