aea60a4f20f457dda5e451475ae9df45c75d9f92
Timofey Solomko <tsolomko@gmail.com>
2017-10-01 11:54:36 +0300

    Update SWCompression to 3.3.1

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,8 +9,8 @@ import SwiftCLI
 
 /* TODO: Switch to usage of Bundle.allBundles() function of Foundation framework when it becomes implemented.*/
 // Version constants:
-let SWCompressionVersion = "3.3.0"
-let swcompRevision = "84"
+let SWCompressionVersion = "3.3.1"
+let swcompRevision = "85"
 
 CLI.setup(name: "swcomp",
           version: "\(swcompRevision), SWCompression version: \(SWCompressionVersion)",
